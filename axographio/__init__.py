50632ddda125a467d1cecfde5c2b091cdee9fd96
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2018-06-08 22:03:45 -0400

    Added smart single-sourced versioning
    These changes allow the package version to be specific in one place instead of two. For development builds, the git revision number will be appended. Code modified from the NumPy project.

--- axographio/__init__.py
+++ axographio/__init__.py
@@ -1,18 +1,29 @@
-__version__ = '0.2.0'
+from .version import version as __version__
+from .version import git_revision as __git_revision__
 
-from .extension import *
+# Detect if this module is being loaded as part of the axographio setup routine
+# (probably to fetch the git_revision variable in the absence of the .git
+# directory), to avoid attempting to load the extension before it is built.
+try:
+    __AXOGRAPHIO_SETUP__
+except NameError:
+    __AXOGRAPHIO_SETUP__ = False
 
-# explicit listing needed to ensure pydoc/help() finds everything
-__all__ = [
-    'file_contents',
-    'linearsequence',
-    'scaledarray',
-    'aslinearsequence',
-    'asscaledarray',
-    'read',
-    'axograph_x_format',
-    'newest_format',
-    'old_digitized_format',
-    'old_graph_format',
-    'supported_formats'
-    ]
+if not __AXOGRAPHIO_SETUP__:
+    # must be running from an installation
+    from .extension import *
+
+    # explicit listing needed to ensure pydoc/help() finds everything
+    __all__ = [
+        'file_contents',
+        'linearsequence',
+        'scaledarray',
+        'aslinearsequence',
+        'asscaledarray',
+        'read',
+        'axograph_x_format',
+        'newest_format',
+        'old_digitized_format',
+        'old_graph_format',
+        'supported_formats'
+        ]
